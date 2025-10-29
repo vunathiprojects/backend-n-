bbe4e318446990bd79c893770597cc4dcb256a96
Monika Morla <111050564+MonikaMorla@users.noreply.github.com>
2025-10-29 16:41:38 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ echo "Starting FastAPI (AI Backend)..."
 uvicorn ai_backend.main:app --host 0.0.0.0 --port 8001 &
 
 echo "Starting Django Backend..."
-gunicorn --bind=0.0.0.0:$PORT config.wsgi
+gunicorn --bind=0.0.0.0:${PORT:-8000} config.wsgi
